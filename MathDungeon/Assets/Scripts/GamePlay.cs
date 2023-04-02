7221a9cd4a6f77c3e2519b97dc84e83650092aa7
bakasibruceto <abla09027@gmail.com>
2023-04-02 19:27:47 +0800

    update

--- MathDungeon/Assets/Scripts/GamePlay.cs
+++ MathDungeon/Assets/Scripts/GamePlay.cs
@@ -7,24 +7,43 @@ using UnityEngine.UI;
 
 
 public class GamePlay : MonoBehaviour
-{
+{   
+    public Keypad key;
     public string num, usernum, tempnum;
     public string[] ops = {"+","x"};   
-    public Text UserInput, num1,num2,op;
-    public string rum1;
-    public string rum2;
-    public int oprand, rand1, rand2;
+    public Text UserInput,num1,num2,op, playerValue, monsterValue;
+    
+    public int oprand, rand1, rand2, rum1, rum2, ans, plyval = 100, mobval = 100;
+
+    private HealthBarScript hp;
+
+    public void randomizer(){
+        rand1 = Random.Range(1,11);
+        oprand = Random.Range(0,2);
+        rand2 = Random.Range(1,11);
+    }
 
     public void Start() {
-        oprand = Random.Range(0,2);
-        rand1 = Random.Range(1,11);
-        rand2 = Random.Range(1,11);
-        rum1 = rand1.ToString();
-        rum2 = rand2.ToString();
-        num1.text = rum1;
-        num2.text = rum2;
+        randomizer();
+        playerValue.text = plyval.ToString();
+        monsterValue.text = mobval.ToString();
+        rum1 = rand1;
+        rum2 = rand2;
+        num1.text = rum1.ToString();
+        num2.text = rum2.ToString();
         op.text = ops[oprand];
+        switch(oprand)
+        {
+            case 0:
+                ans = rand1+rand2;
+                break;
+            case 1:
+                ans = rand1*rand2;  
+                break;  
+        }
     }
+    
+    
     
     public void Update()
     {   
@@ -32,44 +51,7 @@ public class GamePlay : MonoBehaviour
             tempnum = num;  
             usernum = num;
         } 
-        UserInput.text = usernum;
     }
 
-    public void ButtonClear(){
-        num = "";
-        tempnum = "";
-        usernum = "";
-    }
-    public void Button1(){
-        num = tempnum+"1";
-    }
-    public void Button2(){
-        num = tempnum+"2";
-    }
-    public void Button3(){
-        num = tempnum+"3";
-    }
-     public void Button4(){
-        num = tempnum+"4";
-    }
-     public void Button5(){
-        num = tempnum+"5";
-    }
-     public void Button6(){
-        num = tempnum+"6";
-    }
-    public void Button7(){
-        num = tempnum+"7";
-    }
-    public void Button8(){
-        num = tempnum+"8";
-    }
-    public void Button9(){
-        num = tempnum+"9";
-    }
-    public void Button0(){
-        num = tempnum+"0";
-    }
 
-  
 }
---
+++ MathDungeon/Assets/Scripts/Keypad.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypad : MonoBehaviour
+{
+    public GamePlay gp;
+
+    public void Update(){
+        gp.UserInput.text = gp.usernum;
+    }
+
+    public void ButtonEnter(){
+        if (gp.ans == int.Parse(gp.usernum)){
+            gp.mobval -= gp.ans;
+        } else {
+            gp.plyval -= gp.ans;
+        }
+        ButtonRefresh();
+        
+    }
+
+    public void ButtonRefresh(){
+        ButtonClear();
+        gp.randomizer();
+        gp.Start();
+        
+    }
+    public void ButtonClear(){
+        gp.num = "";
+        gp.tempnum = "";
+        gp.usernum = "";
+    }
+    public void Button1(){
+        gp.num = gp.tempnum+"1";
+    }
+    public void Button2(){
+        gp.num = gp.tempnum+"2";
+    }
+    public void Button3(){
+        gp.num = gp.tempnum+"3";
+    }
+     public void Button4(){
+        gp.num = gp.tempnum+"4";
+    }
+     public void Button5(){
+        gp.num = gp.tempnum+"5";
+    }
+     public void Button6(){
+        gp.num = gp.tempnum+"6";
+    }
+    public void Button7(){
+        gp.num = gp.tempnum+"7";
+    }
+    public void Button8(){
+        gp.num = gp.tempnum+"8";
+    }
+    public void Button9(){
+        gp.num = gp.tempnum+"9";
+    }
+    public void Button0(){
+        gp.num = gp.tempnum+"0";
+    }
+
+  
+}
+
+
